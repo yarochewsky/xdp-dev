27b74b77361a446fc2baa35b65e430b0de04e494
Daniel <dfyarochewsky@gmail.com>
2020-03-30 03:20:27 +0000

    - modularized the loader functionality - had A LOT of trouble figuring out how to link the loader.o; Basically, was passing the arguments as such to the linker:
    clang -L. -lbpf... loader.o main.o -o main
    
    when it should be
    
    clang loader.o main.o -L. -lbpf -o main
    
    Because the linker resolves from left to right, noting undefined symbols along the way. The file that needs the symbol comes
    before the one that resolves it.
    
    Since loader.o uses a bunch of libbpf functions, by placing it after libbpf, the linker saw libbf first, which is because it's a static library,
    is self-contained - no problems there. Then it saw loader.o with bunch of symbols that do not define themselves. The linker has nothing after it to
    resolve them - which is also why they suggest including the library twice, at the beginning and end, so that the linked would be able to resolve it then the second time, after it sees loader.o's symbols. Basically, the linked has "short memory" =) btw this second approach is dirty, not going with it - I prefer to just include the object files before libbpf.
    
    https://stackoverflow.com/questions/45135/why-does-the-order-in-which-libraries-are-linked-sometimes-cause-errors-in-gcc

---
+++ xdp/loader/loader.h
@@ -0,0 +1,45 @@
+#ifndef __LOADER_H__
+#define __LOADER_H__
+
+#include <stdlib.h>
+#include <net/if.h>
+#include <linux/if_link.h>
+#include <bpf/bpf.h>
+#include <bpf/libbpf.h>
+
+
+/**
+	load_xdp_object_file: loads an object into obj given a filename and optional
+												hw offload iface index.
+
+	@filename: name of object file to load
+	@ifindex: interface index for hw offload (specify 0 if none)
+	@obj: return param for loaded object
+
+	@returns non-zero for error
+**/
+int load_xdp_object_file(const char* filename, int ifindex, struct bpf_object** obj);
+
+/**
+	xdp_link_detach: detaches the current xdp program from the interface
+
+	@ifindex: interface index of attached program
+	@xdp_flags: flag modes for detaching
+
+	@returns non-zero for error
+**/
+int xdp_link_detach(int ifindex, __u32 xdp_flags);
+
+/**
+	xdp_link_attach: attaches the program in fd to the interface
+	
+	@ifindex: interface index to attach program to
+	@xdp_flags: flag modes for attaching
+	@fd: file descriptor of program to attach
+
+	@returns non-zero for error
+**/
+int xdp_link_attach(int ifindex, __u32 xdp_flags, int fd);
+
+
+#endif // _LOADER_H
--- xdp/main.c
+++ xdp/main.c
@@ -3,80 +3,12 @@
 #include <stdlib.h>
 #include <net/if.h>
 #include <linux/if_link.h>
-#include "stats.h"
 #include <bpf/bpf.h>
 #include <bpf/libbpf.h>
-#include "bpf_util.h"
 
-
-int load_xdp_object_file(const char* filename, int ifindex, struct bpf_object** obj) {
-	int fd = -1;
-	int err;
-
-	struct bpf_prog_load_attr attr = {
-		.prog_type = BPF_PROG_TYPE_XDP,
-		.ifindex = ifindex,
-		.file = filename,
-	};
-
-	err = bpf_prog_load_xattr(&attr, obj, &fd);
-	if (err) {
-		fprintf(stderr, "err: loading ebpf object file(%s): %s\n", filename, strerror(-err));
-		return -1;
-	}
-	return 0;	
-}
-
-int xdp_link_detach(int ifindex, __u32 xdp_flags) {
-	int err;
-
-	if ((err = bpf_set_link_xdp_fd(ifindex, -1, xdp_flags)) < 0) {
-		fprintf(stderr, "err: setting xdp unload failed: %s\n", strerror(-err));
-		return -1;
-	}
-	return 0;
-}
-
-int xdp_link_attach(int ifindex, __u32 xdp_flags, int fd) {
-	int err;
-
-	err = bpf_set_link_xdp_fd(ifindex, fd, xdp_flags);
-	if (err == -EEXIST && !(xdp_flags & XDP_FLAGS_UPDATE_IF_NOEXIST)) {
-		__u32 old_flags = xdp_flags;
-		xdp_flags &= ~XDP_FLAGS_MODES;
-		xdp_flags |= (old_flags & XDP_FLAGS_SKB_MODE) ? XDP_FLAGS_DRV_MODE : XDP_FLAGS_SKB_MODE;
-		err = bpf_set_link_xdp_fd(ifindex, -1, xdp_flags);
-		if (!err) {
-			err = bpf_set_link_xdp_fd(ifindex, fd, old_flags);
-		}
-	}
-
-	if (err < 0) {
-		fprintf(stderr, "err: ifindex(%d) link set xdp fd failed: %s\n", ifindex, strerror(-err));
-		switch (-err) {
-			case EBUSY:
-			case EEXIST:
-				fprintf(stderr, "xdp already loaded\n");
-			case EOPNOTSUPP:
-				fprintf(stderr, "native xdp not supported\n");
-			default:
-				break;
-		}
-		return -1;
-	}
-	return 0;
-}
-
-int find_map_fd(struct bpf_object* bpf_obj, const char* name) {
-	struct bpf_map* map;
-	int fd = -1;
-
-	map = bpf_object__find_map_by_name(bpf_obj, name);
-	if (!map) {
-		fprintf(stderr, "could not find map\n");
-	}
-	return fd;
-}
+#include "loader/loader.h"
+#include "headers/stats.h"
+#include "headers/bpf_util.h"
 
 static void usage() {
 	printf("usage: ./main <elf_filepath> <iface>\n");
